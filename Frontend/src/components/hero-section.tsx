db8469aa5c87ede5cbaf3b7a2afef5815cd5183c
JayShende <shendejay379@gmail.com>
2025-05-22 21:22:04 +0530

    added the registered icon to the hero section

--- Frontend/src/components/hero-section.tsx
+++ Frontend/src/components/hero-section.tsx
@@ -1,19 +1,22 @@
 import VideoHeroSection from "./ui/video-container";
-
+import { FaRegRegistered } from "react-icons/fa";
 const HeroSection = () => {
   return (
-   <div className="w-full">
-    <div className="mt-40 flex justify-between items-end">
-        <div className="text-8xl text-white ml-[44px] font-normal font-Poppins">
+    <div className="w-full">
+      <div className="mt-40 flex justify-between items-end">
+        <div className="flex">
+          <div className="text-8xl text-white ml-[44px] font-normal font-Poppins">
             Eclypse
+          </div>
+          <FaRegRegistered className="text-lg text-white" />
         </div>
         <div className="text-white text-sm font-medium font-Poppins mr-[50.6px]">
-            © 2025
+          © 2025
         </div>
+      </div>
+      <VideoHeroSection />
     </div>
-    <VideoHeroSection/>
-   </div>
-  )
+  );
 };
 
 export default HeroSection;
