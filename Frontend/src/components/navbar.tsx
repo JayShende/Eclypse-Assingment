7e3088f868c306666c9fdc8f87b52a98e5571f2b
JayShende <shendejay379@gmail.com>
2025-05-22 21:17:36 +0530

    added the glassmorph effect to the navbar

--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -3,7 +3,8 @@ import BuyButton from './ui/buy-button';
 
 const NavBarComponent = () => {
   return (
-    <div className="fixed top-0 left-0 w-full z-50 h-[75px] bg-black bg-opacity-60 pl-[60px] pr-[50.62px] pt-[12.9px] pb-[16.6px] flex justify-between items-center backdrop-blur-lg">
+    <div className="fixed top-0 left-0 w-full z-50 h-[75px] bg-black/40 px-16 py-3 flex justify-between items-center backdrop-blur-md backdrop-saturate-150 border-b border-white/10">
+
       <img src={logoNavbar} alt="logo" className="w-[48.79px] h-[45.464px]" />
       <div className="text-white flex gap-x-[70px] font-Poppins text-xl font-normal items-center">
         <span>About Us</span>
